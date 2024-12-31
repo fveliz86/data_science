bb7d3883531873dd7056930e6a3b9380e7b63267
Fernando Véliz <fveliz@payway.com.ar>
2024-12-31 16:58:12 -0300

    update

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-from streamlit_option_menu import option_menu
+# from streamlit_option_menu import option_menu
 
 st.set_page_config(page_title='Fernando Véliz\'s portfolio',layout="wide")
 
